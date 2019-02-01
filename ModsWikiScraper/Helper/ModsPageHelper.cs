b88d52ad63aa7d68f1bc7ac4a69ec95c29bee17f
Kurt Lourens <hi@kurtlourens.com>
2019-02-01 13:22:36 +0200

    Initial Commit

---
+++ ModsWikiScraper/Helper/ModsPageHelper.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
+
+namespace ModsWikiScraper.Helper
+{
+    public static class ModsPageHelper
+    {
+        public static List<string> GetListOfModTypes(this HtmlDocument doc)
+        {
+            var nodes = doc.DocumentNode.Descendants("table")
+                .Select(x => x.ParentNode.ParentNode)
+                .Where(x => x.Attributes["title"] != null)
+                .Select(x => x.Attributes["title"].Value)
+                .ToList();
+            return nodes;
+        }
+
+        public static List<string> GetListOfModTypesWithCounts(this HtmlDocument doc)
+        {
+            var nodes = doc.DocumentNode.Descendants("table")
+                .Select(x => x.ParentNode.ParentNode)
+                .Where(x => x.Attributes["title"] != null)
+                .Select(x => new
+                {
+                    title = x.Attributes["title"].Value,
+                    //count = x.Descendants("tr").Where(tr => tr.Descendants("td").Any()).Count()
+                    count = x.Descendants("tr").Where(tr => tr.Descendants("td").Any()).ToList().Count
+                })
+                .ToList();
+            return nodes.Select(n => $"{n.title} {n.count}").ToList();
+        }
+
+        public static List<string> GetListOfModTypesNames(this HtmlDocument doc)
+        {
+            var nodes = doc.DocumentNode.Descendants("table")
+                .Select(x => x.ParentNode.ParentNode)
+                .Where(x => x.Attributes["title"] != null)
+                .SelectMany(x => x.Descendants("tr").Where(tr => tr.Descendants("td").Any()).ToList())
+                .Select(x => x.FirstChild?.Attributes["data-param"]?.Value ?? "unknown")
+                .ToList();
+            return nodes;//.Select(n => $"{n.InnerText}").ToList();
+        }
+
+    }
+}
---
+++ ModsWikiScraper/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using HtmlAgilityPack;
+using ModsWikiScraper.Helper;
+
+namespace ModsWikiScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var url = "https://warframe.fandom.com/wiki/Mods";
+            var web = new HtmlWeb();
+            var doc = web.Load(url);
+
+            var modTypes = doc.GetListOfModTypes();
+            foreach (string modType in modTypes)
+            {
+                Console.WriteLine(modType);
+            }
+
+            Console.WriteLine("---------------------------------");
+
+            var modTypesWithCount = doc.GetListOfModTypesWithCounts();
+            foreach (string modTypeWithCount in modTypesWithCount)
+            {
+                Console.WriteLine(modTypeWithCount);
+            }
+        }
+    }
+}
